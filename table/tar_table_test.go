8d58482ca812a865a3625a2c01086e292bc5481b
jillian <jillian@thecape.ca>
2016-04-13 22:20:14 -0300

    new test

--- table/tar_table_test.go
+++ table/tar_table_test.go
@@ -130,3 +130,69 @@ func TestTarRaises(t *testing.T) {
 	// 	t.Fatal(err)
 	// }
 }
+
+func TestTableSetup(t *testing.T) {
+	t.Parallel()
+
+	opts := table.Config{
+		Game: table.Tarabish,
+		// Stakes: table.Stakes{
+		// 	SmallBet: 1,
+		// 	BigBet:   2,
+		// 	Ante:     0,
+		// },
+		NumOfSeats: 4,
+	}
+
+	p1 := Player("1", []PlayerAction{})
+	p2 := Player("2", []PlayerAction{})
+	p3 := Player("3", []PlayerAction{})
+	p4 := Player("4", []PlayerAction{})
+
+	tbl := table.New(opts, hand.NewDealer())
+
+	if err := tbl.Sit(p1, table.Parameters{Seat:0}); err != nil {
+		t.Fatal(err)
+	}
+	if err := tbl.Sit(p2, table.Parameters{Seat:1}); err != nil {
+		t.Fatal(err)
+	}
+	if err := tbl.Sit(p3, table.Parameters{Seat:2}); err != nil {
+		t.Fatal(err)
+	}
+	if err := tbl.Sit(p4, table.Parameters{Seat:3}); err != nil {
+		t.Fatal(err)
+	}
+    
+    //bidding
+    p1.Bid("pass")
+    p2.Bid("pass")
+
+	for i := 0; i < 2; i++ {
+		if _, _, err := tbl.Next(); err != nil {
+			t.Fatal(err)
+		}else{
+            t.Logf("Next succeeded : action is %v number of seats is %d and %d",
+            tbl.Action(),
+            tbl.NumOfSeats(), 
+            tbl.Round(),
+            )
+        }
+	}
+
+	if tbl.Action() != 1 {
+		t.Fatal("action should be on player 2",tbl.Action())
+	}
+
+	players := tbl.Players()
+	if players[1].CanRaise() {
+		t.Fatal("player 2 shouldn't be able to raise")
+	}
+
+	// p2.Call()
+	// _, _, err := tbl.Next()
+	// _, _, err = tbl.Next()
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+}
